322a2b0d75936b0f81dddcdd3a246d9216ecf828
Sebastian <Sebastian.veile@hotmail.com>
2021-07-27 17:13:40 +0200

    Add function to calculate both black and white pawn moves. Also add test for edgecases and additional refactoring

--- src/chess_game/engine/moves.clj
+++ src/chess_game/engine/moves.clj
@@ -1,32 +1,100 @@
 (ns chess-game.engine.moves
   (:require [chess-game.engine.direction :as direction]))
 
+;;TODO
+;;Rooks
+;;Bishops
+;;Queens
+;;Promotion of pawn
+;;Concept of check - and not being able to move intgvf o check as king
+;;En Pessant
+;;Castling
+
 "Bitboard where all bits are 1 except for the given file.
  Used to clip moves, so they wont jump to the other side of the board"
 (def not-A-file (unchecked-long 0x7f7f7f7f7f7f7f7f))
 (def not-H-file (unchecked-long 0xfefefefefefefefe))
+(def rank-3 (unchecked-long 0xff0000))
+(def rank-6 (unchecked-long 0xff0000000000))
 (def not-AB-file (unchecked-long 0x3f3f3f3f3f3f3f3f))       ;; Used for knights
 (def not-HG-file (unchecked-long 0xfcfcfcfcfcfcfcfc))       ;; Used for knights
 
-;;Bit-shift-left is for positive directions. Right is for negative directions
-#_(defn white-possible-pawn-moves
-  [history bitboards]
-  (let [white-pawns-bboard (get-in bitboards [:white :P])
-        black-pieces-bboard (get-in bitboards [:occupancy :black])
+(defn find-white-pawn-moves [white-pawn-bboard all-pieces black-pieces]
+  "White pawns can only move forward if there are no white and black pieces on those positions.
+   White pawns can only attacks north-west or north-east if there is a black piece on that position
+
+           2
+        3  1  4
+           P
+
+
+   "
+  (let [empty-slots (bit-not all-pieces)
+
+        ;;Checks if the step in front of the pawn is not occupied by other pieces
+        white-pawn-single-push (bit-and (direction/north-one white-pawn-bboard) empty-slots)
+
+        ;;Checks if the pawn can move an additional step based on the results of white-pawn-single-push
+        ;;If the pawn is on rank 3 and the space in front is free, it can move again.
+        white-pawn-double-push (bit-and (direction/north-one (bit-and white-pawn-single-push rank-3)) empty-slots)
+
+        ;;Union both to get all possible forward pawn moves
+        white-pawn-push (bit-or white-pawn-single-push white-pawn-double-push)
 
         ;;White attack diagonal right
-        attacks-ne (bit-and (direction/north-east-one white-pawns-bboard) black-pieces-bboard not-A-file)
+        attacks-ne (bit-and (direction/north-east-one white-pawn-bboard) not-A-file)
 
         ;;White attack diagonal left
-        attacks-nw (bit-and (direction/north-west-one white-pawns-bboard) black-pieces-bboard not-H-file)
+        attacks-nw (bit-and (direction/north-west-one white-pawn-bboard) not-H-file)
 
-        ;;White push single
+        ;;Union both to get all possible attack moves
+        white-attack-moves (bit-and black-pieces (bit-or attacks-ne attacks-nw))
 
-        ;;White push double
+        ;;TODO En passant moves
+        ]
 
-        ]))
+    ;;Union white pawn attacks and pushes
+    (bit-or white-pawn-push white-attack-moves)))
 
-(defn white-king-moves [bitboards]
+(defn find-black-pawn-moves [black-pawn-bboard all-pieces white-pieces]
+  "Black pawns can only move forward if there are no white and black pieces on those positions.
+   Black pawns can only attacks north-west or north-east if there is a white piece on that position
+
+
+           P
+         4 1 3
+           2
+
+
+   "
+  (let [empty-slots (bit-not all-pieces)
+
+        ;;Checks if the step in front of the pawn is not occupied by other pieces
+        black-pawn-single-push (bit-and (direction/south-one black-pawn-bboard) empty-slots)
+
+        ;;Checks if the pawn can move an additional step based on the results of black-pawn-single-push
+        ;;If the pawn is on rank 3 and the space in front is free, it can move again.
+        black-pawn-double-push (bit-and (direction/south-one (bit-and black-pawn-single-push rank-6)) empty-slots)
+
+        ;;Union both to get all possible forward pawn moves
+        white-pawn-push (bit-or black-pawn-single-push black-pawn-double-push)
+
+        ;;Black attack diagonal right
+        attacks-ne (bit-and (direction/south-east-one black-pawn-bboard) not-A-file)
+
+        ;;Black attack diagonal left
+        attacks-nw (bit-and (direction/south-west-one black-pawn-bboard) not-H-file)
+
+        ;;Union both to get all possible attack moves
+        white-attack-moves (bit-and white-pieces (bit-or attacks-ne attacks-nw))
+
+        ;;TODO En passant moves
+        ]
+
+    ;;Union white pawn attacks and pushes
+    (bit-or white-pawn-push white-attack-moves)))
+
+(defn find-king-moves [king-bboard own-pieces-bboard]
   "White king can move in 8 different directions
 
 
@@ -36,35 +104,32 @@
 
 
   "
-  (let [white-king-bboard (get-in bitboards [:white :K])
-        white-pieces-bboard (get-in bitboards [:occupancy :white])
-        black-pieces-bboard (get-in bitboards [:occupancy :black])
-
-        ;;Masks the board if the king is on either the A or H.
+  (let [;;Masks the board if the king is on either the A or H.
         ;;If the king is on A, we don't want to calculate positions to the left and vice versa.
-        king-mask-file-A-bboard (bit-and white-king-bboard not-A-file)
-        king-mask-file-H-bboard (bit-and white-king-bboard not-H-file)
+        king-mask-file-A-bboard (bit-and king-bboard not-A-file)
+        king-mask-file-H-bboard (bit-and king-bboard not-H-file)
 
         ;Sets a bit in all 8 directions if possible
         move-ne (direction/north-east-one king-mask-file-H-bboard)
-        move-n (direction/north-one white-king-bboard)
+        move-n (direction/north-one king-bboard)
         move-nw (direction/north-west-one king-mask-file-A-bboard)
         move-w (direction/west-one king-mask-file-A-bboard)
         move-sw (direction/south-west-one king-mask-file-A-bboard)
-        move-s (direction/south-one white-king-bboard)
+        move-s (direction/south-one king-bboard)
         move-se (direction/south-east-one king-mask-file-H-bboard)
         move-e (direction/east-one king-mask-file-H-bboard)
 
         ;;Union all possible moves and remove moves where white already has pieces
-        king-moves (bit-and (bit-not white-pieces-bboard) (bit-or move-ne move-nw move-w
-                                                                  move-sw move-se move-e
-                                                                  move-n move-s))
+        king-moves (bit-and (bit-not own-pieces-bboard) (bit-or move-ne move-nw move-w
+                                                                move-sw move-se move-e
+                                                                move-n move-s))
 
-        ;;Remove moves that will result in check.
+        ;;TODO Remove moves that will result in check.
+
         ]
     king-moves))
 
-(defn get-knight-moves [bitboards]
+(defn find-knight-moves [knight-bboard own-pieces-bboard]
   "Knights can move in 8 different directions
 
           2       3
@@ -75,24 +140,41 @@
 
 
   "
-  (let [;;There is no distinction between black and white knight moves,
-        ;; therefore we only need to decide from what color we extract the bit boards
-        white-knight-bboard (unchecked-long (get-in bitboards [:white :N]))
-        white-pieces-bboard (unchecked-long (get-in bitboards [:occupancy :white]))
+  (let [;;Calculates the position in all 8 directions
+        knight-no-e-e (direction/north-east-east (bit-and knight-bboard not-HG-file))
+        knight-no-no-e (direction/north-north-east (bit-and knight-bboard not-H-file))
+        knight-no-no-w (direction/north-north-west (bit-and knight-bboard not-A-file))
+        knight-no-w-w (direction/north-west-west (bit-and knight-bboard not-AB-file))
+        knight-so-w-w (direction/south-west-west (bit-and knight-bboard not-AB-file))
+        knight-so-so-w (direction/south-south-west (bit-and knight-bboard not-A-file))
+        knight-so-so-e (direction/south-south-east (bit-and knight-bboard not-H-file))
+        knight-so-e-e (direction/south-east-east (bit-and knight-bboard not-HG-file))]
 
-        ;;Calculates the position in all 8 directions
-        knight-no-e-e (direction/north-east-east (bit-and white-knight-bboard not-HG-file))
-        knight-no-no-e (direction/north-north-east (bit-and white-knight-bboard not-H-file))
-        knight-no-no-w (direction/north-north-west (bit-and white-knight-bboard not-A-file))
-        knight-no-w-w (direction/north-west-west (bit-and white-knight-bboard not-AB-file))
-        knight-so-w-w (direction/south-west-west (bit-and white-knight-bboard not-AB-file))
-        knight-so-so-w (direction/south-south-west (bit-and white-knight-bboard not-A-file))
-        knight-so-so-e (direction/south-south-east (bit-and white-knight-bboard not-H-file))
-        knight-so-e-e (direction/south-east-east (bit-and white-knight-bboard not-HG-file))
+    ;;Union all possible moves and remove moves where white already has pieces
+    (bit-and (bit-not own-pieces-bboard)
+             (bit-or knight-no-e-e knight-no-no-e knight-no-no-w knight-no-w-w
+                     knight-so-e-e knight-so-so-e knight-so-so-w knight-so-w-w))))
 
-        ;;Union all possible moves and remove moves where white already has pieces
-        knight-moves (bit-and (bit-not white-pieces-bboard)
-                              (bit-or knight-no-e-e knight-no-no-e knight-no-no-w knight-no-w-w
-                                      knight-so-e-e knight-so-so-e knight-so-so-w knight-so-w-w))
-        ]
-    knight-moves))
+(defn white-turn? [bitboards]
+  (get-in bitboards [:history :turn]))
+
+
+(defn get-moves [bitboards]
+  "Just a temp function to illustrate that the king and knight functions are generic and works for both colors
+  This is not true for the pawn functions."
+  (if (white-turn? bitboards)
+    (let [occupancy (:occupancy bitboards)
+          white-pieces (:white occupancy)
+
+          king-moves (find-king-moves (get-in bitboards [:white :K]) white-pieces)
+          knight-moves (find-knight-moves (get-in bitboards [:white :N]) white-pieces)
+          pawn-moves (find-white-pawn-moves (get-in bitboards [:white :P]) (:all occupancy) (:black occupancy))]
+      (bit-or king-moves knight-moves pawn-moves))
+    (let [occupancy (:occupancy bitboards)
+          black-pieces (:black occupancy)
+
+          king-moves (find-king-moves (get-in bitboards [:black :k]) black-pieces)
+          knight-moves (find-knight-moves (get-in bitboards [:black :n]) black-pieces)
+          pawn-moves (find-white-pawn-moves (get-in bitboards [:black :p]) (:all occupancy) (:white occupancy))]
+      (bit-or king-moves knight-moves pawn-moves)))
+  )
--- test/chess_game/engine/moves_test.clj
+++ test/chess_game/engine/moves_test.clj
@@ -3,70 +3,128 @@
             [chess-game.engine.moves :as move]))
 
 
-(deftest king-moves
+(deftest king-moves-test
   "Validating king moves from different positions"
   (testing "Testing king pos H4"
-    (is (= (move/white-king-moves {:white     {:K 16777216}
-                                   :occupancy {:white 1
-                                               :black 2048}})
+    (is (= (move/find-king-moves 16777216 1)
            12918652928)))
 
   (testing "King position H1 (First bit)"
-    (is (= (move/white-king-moves {:white     {:K (unchecked-long 1)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+    (is (= (move/find-king-moves (unchecked-long 1) 0)
            (unchecked-long 770))))
 
   (testing "King position A1"
-    (is (= (move/white-king-moves {:white     {:K (unchecked-long 128)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+    (is (= (move/find-king-moves (unchecked-long 128) 0)
            (unchecked-long 49216))))
 
   (testing "King position H8"
-    (is (= (move/white-king-moves {:white     {:K (unchecked-long 72057594037927936)}
-                                   :occupancy {:white 0
-                                               :black 0}})
+    (is (= (move/find-king-moves (unchecked-long 72057594037927936) 0)
            (unchecked-long 144959613005987840))))
 
   (testing "King position A8 (Last bit)"
-    (is (= (move/white-king-moves {:white     {:K (unchecked-long 9223372036854775808)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+    (is (= (move/find-king-moves (unchecked-long 9223372036854775808) 0)
            (unchecked-long 4665729213955833856)))))
 
-(deftest knight-moves
-  "Validating king moves from different positions"
-  (testing "King position H1 (First bit)"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 1)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+(deftest knight-moves-test
+  "Validating knight moves from different positions"
+  (testing "Knight position H1 (First bit)"
+    (is (= (move/find-knight-moves (unchecked-long 1) 0)
            (unchecked-long 132096))))
 
-  (testing "King position A1"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 128)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+  (testing "Knight position A1"
+    (is (= (move/find-knight-moves (unchecked-long 128) 0)
            (unchecked-long 4202496))))
 
-  (testing "King position H8"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 72057594037927936)}
-                                   :occupancy {:white 0
-                                               :black 0}})
+  (testing "Knight position H8"
+    (is (= (move/find-knight-moves (unchecked-long 72057594037927936) 0)
            (unchecked-long 1128098930098176))))
 
-  (testing "King position A8 (Last bit)"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 9223372036854775808)}
-                                   :occupancy {:white 0
-                                               :black 2048}})
+  (testing "Knight position A8 (Last bit)"
+    (is (= (move/find-knight-moves (unchecked-long 9223372036854775808) 0)
            (unchecked-long 9077567998918656))))
+
   (testing "Knight position D4"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 268435456)}
-                                   :occupancy {:white (unchecked-long 0)
-                                               :black 2048}})
+    (is (= (move/find-knight-moves (unchecked-long 268435456) (unchecked-long 0))
            (unchecked-long 44272527353856))))
+
   (testing "Knight position D4, but all slots are filled by white pieces"
-    (is (= (move/get-knight-moves {:white     {:N (unchecked-long 268435456)}
-                                   :occupancy {:white (unchecked-long 44272527353856)
-                                               :black 2048}})
-           (unchecked-long 0)))))
+    (is (= (move/find-knight-moves (unchecked-long 268435456) (unchecked-long 44272527353856))
+           (unchecked-long 0))))
+
+  (testing "Knight at all position D4, A1, H1, H8, A8"
+    (is (= (move/find-knight-moves (unchecked-long 9295429631161139329) 0)
+           (unchecked-long 10249939456502784)))))
+
+(deftest white-pawn-moves-test
+  (testing "initial pawn position - single and double push"
+    (is (= (move/find-white-pawn-moves (unchecked-long 65280) (unchecked-long 65280) 0)
+           (unchecked-long 4294901760))))
+
+  (testing "pawn push on rank other rank than 2"
+    (is (= (move/find-white-pawn-moves (unchecked-long 16711680) (unchecked-long 16711680) 0)
+           (unchecked-long 4278190080))))
+
+  (testing "Pawn position h2 is unable to attack black piece on a2"
+    (is (= (move/find-white-pawn-moves (unchecked-long 256) (unchecked-long 33024) 32768)
+           (unchecked-long 16842752))))
+
+  (testing "Pawn position h2 is able to attack black piece on g3"
+    (is (= (move/find-white-pawn-moves (unchecked-long 256) (unchecked-long 131328) 131072)
+           (unchecked-long 16973824))))
+
+  (testing "Pawn position a2 is unable to attack black piece on h4"
+    (is (= (move/find-white-pawn-moves (unchecked-long 32768) (unchecked-long 16809984) 16777216)
+           (unchecked-long 2155872256))))
+
+  (testing "Pawn position a2 is able to attack black piece on b3"
+    (is (= (move/find-white-pawn-moves (unchecked-long 32768) (unchecked-long 4227072) 4194304)
+           (unchecked-long 2160066560))))
+
+  (testing "Initial pawn position - pawn can only move 1 if piece is in the way on rank 4"
+    (is (= (move/find-white-pawn-moves (unchecked-long 16384) (unchecked-long 1073758208) 0)
+           (unchecked-long 4194304))))
+
+  (testing "Initial pawn position - pawn cannot move since piece is in the way on rank 3"
+    (is (= (move/find-white-pawn-moves (unchecked-long 16384) (unchecked-long 4210688) 0)
+           (unchecked-long 0))))
+
+  (testing "Pawn position b2 can attack black pieces on both diagonals"
+    (is (= (move/find-white-pawn-moves (unchecked-long 16384) (unchecked-long 10502144) 10485760)
+           (unchecked-long 1088421888)))))
+
+(deftest black-pawn-moves-test
+  (testing "initial pawn position - single and double push"
+    (is (= (move/find-black-pawn-moves (unchecked-long 71776119061217280) (unchecked-long 71776119061217280) 0)
+           (unchecked-long 281470681743360))))
+
+  (testing "pawn push on rank other rank than 7"
+    (is (= (move/find-black-pawn-moves (unchecked-long 280375465082880) (unchecked-long 280375465082880) 0)
+           (unchecked-long 1095216660480))))
+
+  (testing "Pawn position h7 is unable to attack black piece on a7"
+    (is (= (move/find-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 36310271995674624) (unchecked-long 36028797018963968))
+           (unchecked-long 1103806595072))))
+
+  (testing "Pawn position h2 is able to attack black piece on g6"
+    (is (= (move/find-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 283673999966208) (unchecked-long 2199023255552))
+           (unchecked-long 3302829850624))))
+
+  (testing "Pawn position a7 is unable to attack black piece on h5"
+    (is (= (move/find-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36028801313931264) 4294967296)
+           (unchecked-long 141287244169216))))
+
+  (testing "Pawn position a7 is able to attack black piece on b6"
+    (is (= (move/find-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36099165763141632) (unchecked-long 70368744177664))
+           (unchecked-long 211655988346880))))
+
+  (testing "Initial pawn position - pawn can only move 1 if piece is in the way on rank 4"
+    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18014673387388928) 0)
+           (unchecked-long 70368744177664))))
+
+  (testing "Initial pawn position - pawn cannot move since piece is in the way on rank 3"
+    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18084767253659648) 0)
+           (unchecked-long 0))))
+
+  (testing "Pawn position b7 can attack black pieces on both diagonals"
+    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18190320369926144) 175921860444160)
+           (unchecked-long 246565482528768)))))
