9537a3270b443d110230dccb77013414bbad284e
Sebastian <Sebastian.veile@hotmail.com>
2021-10-03 15:25:23 +0200

    Implement bishop move lookup functionality

--- src/chess_game/engine/moves.clj
+++ src/chess_game/engine/moves.clj
@@ -34,6 +34,37 @@
                  5 (unchecked-long 0Xff0000000000)          ; 6
                  6 (unchecked-long 0Xff000000000000)        ; 7
                  7 (unchecked-long 0Xff00000000000000)})
+(def diagonal-masks {0  (unchecked-long 0x1)
+                     1  (unchecked-long 0x102)
+                     2  (unchecked-long 0x10204)
+                     3  (unchecked-long 0x1020408)
+                     4  (unchecked-long 0x102040810)
+                     5  (unchecked-long 0x10204081020)
+                     6  (unchecked-long 0x1020408102040)
+                     7  (unchecked-long 0x102040810204080)
+                     8  (unchecked-long 0x204081020408000)
+                     9  (unchecked-long 0x408102040800000)
+                     10 (unchecked-long 0x810204080000000)
+                     11 (unchecked-long 0x1020408000000000)
+                     12 (unchecked-long 0x2040800000000000)
+                     13 (unchecked-long 0x4080000000000000)
+                     14 (unchecked-long 0x8000000000000000)})
+(def anti-diagonal-masks {0  (unchecked-long 0X80)
+                          1  (unchecked-long 0X8040)
+                          2  (unchecked-long 0X804020)
+                          3  (unchecked-long 0X80402010)
+                          4  (unchecked-long 0X8040201008)
+                          5  (unchecked-long 0x804020100804)
+                          6  (unchecked-long 0x80402010080402)
+                          7  (unchecked-long 0x8040201008040201)
+                          8  (unchecked-long 0x4020100804020100)
+                          9  (unchecked-long 0x2010080402010000)
+                          10 (unchecked-long 0x1008040201000000)
+                          11 (unchecked-long 0x804020100000000)
+                          12 (unchecked-long 0x402010000000000)
+                          13 (unchecked-long 0x201000000000000)
+                          14 (unchecked-long 0x100000000000000)})
+
 (def full-board-mask (unchecked-long 0xffffffffffffffff))
 
 
@@ -210,6 +241,14 @@ for further understanding: https://www.youtube.com/watch?v=bCH4YK6oq8M
         vertical-attacks (calculate-ray-moves rook-piece-bitboard occupancy (get file-masks (mod rook-piece-pos 8)))]
 
     (bit-and (bit-or horisontal-attacks vertical-attacks)
+             (bit-not own-pieces))))
+
+(defn find-bishop-moves [^Integer bishop-piece-pos own-pieces occupancy]
+  "Using same technique as rooks"
+  (let [bishop-piece-bitboard (unchecked-long (bit-shift-left 1 bishop-piece-pos))
+        diagonal-attacks (calculate-ray-moves bishop-piece-bitboard occupancy (get diagonal-masks (+ (quot bishop-piece-pos 8) (mod bishop-piece-pos 8))))
+        anti-diagonal-attacks (calculate-ray-moves bishop-piece-bitboard occupancy (get anti-diagonal-masks (- (+ 7 (quot bishop-piece-pos 8)) (mod bishop-piece-pos 8))))]
+    (bit-and (bit-or diagonal-attacks anti-diagonal-attacks)
              (bit-not own-pieces))))
 
 (defn white-turn? [bitboards]
