296fe367b2d8030b56e8838ba781f7fe18b0c32e
Sebastian <Sebastian.veile@hotmail.com>
2022-01-22 19:26:27 +0100

    Refactor docstrings and function names

--- src/chess_game/engine/moves.clj
+++ src/chess_game/engine/moves.clj
@@ -1,15 +1,16 @@
 (ns chess-game.engine.moves
+  "Contains the functionality of generating all possible moves for all 6 piece types."
   (:require [chess-game.engine.direction :as direction]))
 
 (def not-on-A-file (unchecked-long 0x7f7f7f7f7f7f7f7f))
 (def not-on-H-file (unchecked-long 0xfefefefefefefefe))
 (def on-rank-3 (unchecked-long 0xff0000))
 (def on-rank-6 (unchecked-long 0xff0000000000))
-(def not-on-AB-file (unchecked-long 0x3f3f3f3f3f3f3f3f))    ;; Used for knights
-(def not-on-HG-file (unchecked-long 0xfcfcfcfcfcfcfcfc))    ;; Used for knights
+(def not-on-AB-file (unchecked-long 0x3f3f3f3f3f3f3f3f))    ; Used for knights
+(def not-on-HG-file (unchecked-long 0xfcfcfcfcfcfcfcfc))    ; Used for knights
 
 (defn lookup-white-pawn-moves [white-pawn all-pieces black-pieces]
-  "Given a bitboard for representing one or multiple white pawns,
+  "Given a bitboard representing one or multiple white pawns,
   a bitboard representing all-pieces, and a bitboard representing black-pieces,
   return a bitboard where all set bits represent a possible white-pawn move"
 
@@ -17,7 +18,7 @@
 
         single-push (bit-and (direction/move-north-one white-pawn) empty-slots)
 
-        ;If a pawn is on rank 3 after a single-push, it can move an additional step
+        ; If a pawn is on rank 3 after a single-push, it can move an additional step
         double-push (bit-and (direction/move-north-one (bit-and single-push on-rank-3)) empty-slots)
 
         attacks-diagonal-right (bit-and (direction/move-north-east-one white-pawn) not-on-A-file)
@@ -25,14 +26,14 @@
         combined-attack-moves (bit-and black-pieces (bit-or attacks-diagonal-right attacks-diagonal-left))]
     (bit-or single-push double-push combined-attack-moves)))
 
-(defn find-black-pawn-moves [black-pawn all-pieces white-pieces]
-  "Given a bitboard for representing one or multiple black pawns,
+(defn lookup-black-pawn-moves [black-pawn all-pieces white-pieces]
+  "Given a bitboard representing one or multiple black pawns,
   a bitboard representing all-pieces, and a bitboard representing white-pieces,
   return a bitboard where all set bits represent a possible black-pawn move"
   (let [empty-slots (bit-not all-pieces)
         single-push (bit-and (direction/move-south-one black-pawn) empty-slots)
 
-        ;If a pawn is on rank 6 after a single-push, it can move an additional step
+        ; If a pawn is on rank 6 after a single-push, it can move an additional step
         double-push (bit-and (direction/move-south-one (bit-and single-push on-rank-6)) empty-slots)
 
         attacks-diagonal-right (bit-and (direction/move-south-east-one black-pawn) not-on-A-file)
@@ -41,13 +42,13 @@
         combined-attack-moves (bit-and white-pieces (bit-or attacks-diagonal-right attacks-diagonal-left))]
     (bit-or single-push double-push combined-attack-moves)))
 
-(defn find-king-moves [king own-pieces]
-  "Given a bitboard for representing one or multiple kings and a bitboard representing all own-pieces
+(defn lookup-king-moves [king own-pieces]
+  "Given a bitboard representing one or multiple kings and a bitboard representing all own-pieces
   return a bitboard where all set bits represent a possible king move"
   (let [king-not-on-A-file (bit-and king not-on-A-file)
         king-not-on-H-file (bit-and king not-on-H-file)
 
-        ;Sets a bit in all 8 directions if possible
+        ; Sets a bit in all 8 directions if possible
         move-north-east-one (direction/move-north-east-one king-not-on-H-file)
         move-north-one (direction/move-north-one king)
         move-north-west-one (direction/move-north-west-one king-not-on-A-file)
@@ -63,8 +64,8 @@
                                      (bit-not own-pieces))]
     combined-king-moves))
 
-(defn find-knight-moves [knight-bboard own-pieces-bboard]
-  "Given a bitboard for representing one or multiple knights and a bitboard representing all own-pieces
+(defn lookup-knight-moves [knight-bboard own-pieces-bboard]
+  "Given a bitboard representing one or multiple knights and a bitboard representing all own-pieces
   return a bitboard where all set bits represent a possible knight move"
   (let [knight-not-on-AB-file (bit-and knight-bboard not-on-AB-file)
         knight-not-on-HG-file (bit-and knight-bboard not-on-HG-file)
@@ -80,7 +81,7 @@
         move-south-south-east (direction/south-south-east knight-not-on-H-file)
         move-south-east-east (direction/south-east-east knight-not-on-HG-file)]
 
-    ;;Union all possible moves and remove moves where white already has pieces
+    ; Union all possible moves and remove moves where own pieces are located
     (bit-and (bit-not own-pieces-bboard)
              (bit-or move-north-east-east move-north-north-east move-north-north-west move-north-west-west
                      move-south-east-east move-south-south-east move-south-south-west move-south-west-west))))
--- test/chess_game/engine/moves_test.clj
+++ test/chess_game/engine/moves_test.clj
@@ -2,57 +2,56 @@
   (:require [clojure.test :refer :all]
             [chess-game.engine.moves :as move]))
 
-
 (deftest king-moves-test
   "Validating king moves from different positions"
   (testing "Testing king pos H4"
-    (is (= (move/find-king-moves 16777216 1)
+    (is (= (move/lookup-king-moves 16777216 1)
            12918652928)))
 
   (testing "King position H1 (First bit)"
-    (is (= (move/find-king-moves (unchecked-long 1) 0)
+    (is (= (move/lookup-king-moves (unchecked-long 1) 0)
            (unchecked-long 770))))
 
   (testing "King position A1"
-    (is (= (move/find-king-moves (unchecked-long 128) 0)
+    (is (= (move/lookup-king-moves (unchecked-long 128) 0)
            (unchecked-long 49216))))
 
   (testing "King position H8"
-    (is (= (move/find-king-moves (unchecked-long 72057594037927936) 0)
+    (is (= (move/lookup-king-moves (unchecked-long 72057594037927936) 0)
            (unchecked-long 144959613005987840))))
 
   (testing "King position A8 (Last bit)"
-    (is (= (move/find-king-moves (unchecked-long 9223372036854775808) 0)
+    (is (= (move/lookup-king-moves (unchecked-long 9223372036854775808) 0)
            (unchecked-long 4665729213955833856)))))
 
 (deftest knight-moves-test
   "Validating knight moves from different positions"
   (testing "Knight position H1 (First bit)"
-    (is (= (move/find-knight-moves (unchecked-long 1) 0)
+    (is (= (move/lookup-knight-moves (unchecked-long 1) 0)
            (unchecked-long 132096))))
 
   (testing "Knight position A1"
-    (is (= (move/find-knight-moves (unchecked-long 128) 0)
+    (is (= (move/lookup-knight-moves (unchecked-long 128) 0)
            (unchecked-long 4202496))))
 
   (testing "Knight position H8"
-    (is (= (move/find-knight-moves (unchecked-long 72057594037927936) 0)
+    (is (= (move/lookup-knight-moves (unchecked-long 72057594037927936) 0)
            (unchecked-long 1128098930098176))))
 
   (testing "Knight position A8 (Last bit)"
-    (is (= (move/find-knight-moves (unchecked-long 9223372036854775808) 0)
+    (is (= (move/lookup-knight-moves (unchecked-long 9223372036854775808) 0)
            (unchecked-long 9077567998918656))))
 
   (testing "Knight position D4"
-    (is (= (move/find-knight-moves (unchecked-long 268435456) (unchecked-long 0))
+    (is (= (move/lookup-knight-moves (unchecked-long 268435456) (unchecked-long 0))
            (unchecked-long 44272527353856))))
 
   (testing "Knight position D4, but all slots are filled by white pieces"
-    (is (= (move/find-knight-moves (unchecked-long 268435456) (unchecked-long 44272527353856))
+    (is (= (move/lookup-knight-moves (unchecked-long 268435456) (unchecked-long 44272527353856))
            (unchecked-long 0))))
 
   (testing "Knight at all position D4, A1, H1, H8, A8"
-    (is (= (move/find-knight-moves (unchecked-long 9295429631161139329) 0)
+    (is (= (move/lookup-knight-moves (unchecked-long 9295429631161139329) 0)
            (unchecked-long 10249939456502784)))))
 
 (deftest white-pawn-moves-test
@@ -94,37 +93,37 @@
 
 (deftest black-pawn-moves-test
   (testing "initial pawn position - single and double push"
-    (is (= (move/find-black-pawn-moves (unchecked-long 71776119061217280) (unchecked-long 71776119061217280) 0)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 71776119061217280) (unchecked-long 71776119061217280) 0)
            (unchecked-long 281470681743360))))
 
   (testing "pawn push on rank other rank than 7"
-    (is (= (move/find-black-pawn-moves (unchecked-long 280375465082880) (unchecked-long 280375465082880) 0)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 280375465082880) (unchecked-long 280375465082880) 0)
            (unchecked-long 1095216660480))))
 
   (testing "Pawn position h7 is unable to attack black piece on a7"
-    (is (= (move/find-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 36310271995674624) (unchecked-long 36028797018963968))
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 36310271995674624) (unchecked-long 36028797018963968))
            (unchecked-long 1103806595072))))
 
   (testing "Pawn position h2 is able to attack black piece on g6"
-    (is (= (move/find-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 283673999966208) (unchecked-long 2199023255552))
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 281474976710656) (unchecked-long 283673999966208) (unchecked-long 2199023255552))
            (unchecked-long 3302829850624))))
 
   (testing "Pawn position a7 is unable to attack black piece on h5"
-    (is (= (move/find-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36028801313931264) 4294967296)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36028801313931264) 4294967296)
            (unchecked-long 141287244169216))))
 
   (testing "Pawn position a7 is able to attack black piece on b6"
-    (is (= (move/find-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36099165763141632) (unchecked-long 70368744177664))
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 36028797018963968) (unchecked-long 36099165763141632) (unchecked-long 70368744177664))
            (unchecked-long 211655988346880))))
 
   (testing "Initial pawn position - pawn can only move 1 if piece is in the way on rank 4"
-    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18014673387388928) 0)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18014673387388928) 0)
            (unchecked-long 70368744177664))))
 
   (testing "Initial pawn position - pawn cannot move since piece is in the way on rank 3"
-    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18084767253659648) 0)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18084767253659648) 0)
            (unchecked-long 0))))
 
   (testing "Pawn position b7 can attack black pieces on both diagonals"
-    (is (= (move/find-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18190320369926144) 175921860444160)
+    (is (= (move/lookup-black-pawn-moves (unchecked-long 18014398509481984) (unchecked-long 18190320369926144) 175921860444160)
            (unchecked-long 246565482528768)))))
